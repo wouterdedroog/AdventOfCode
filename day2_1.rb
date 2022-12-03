2dcd83c826ce6696a8770c5996f428ec2dbbbee3
MrWouterNL <wouter@dedroog.com>
2022-12-03 12:15:34 +0100

    Add day2 solutions

---
+++ day2_1.rb
@@ -0,0 +1,31 @@
+lines = File.open('inputs/day2.txt').readlines
+
+definitions = {
+  'A' => 'Rock',
+  'B' => 'Paper',
+  'C' => 'Scissors',
+  'X' => 'Rock',
+  'Y' => 'Paper',
+  'Z' => 'Scissors'
+}
+
+strategies = {
+  'Rock' => 'Paper',
+  'Paper' => 'Scissors',
+  'Scissors' => 'Rock'
+}
+
+points = 0
+lines.each_with_index do |line, index|
+  opponent = definitions[line.chars[0]]
+  response = definitions[line.chars[2]]
+
+  points += strategies.find_index { |k,_| k == response } + 1 # add points based on the index of the move I played
+
+  if opponent === response # draw
+    points += 3
+  elsif strategies[opponent] == response # i won
+    points += 6
+  end
+end
+p points
---
+++ day2_2.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+lines = File.open('inputs/day2.txt').readlines
+
+definitions = {
+  'A' => 'Rock',
+  'B' => 'Paper',
+  'C' => 'Scissors',
+  'X' => 'Lose',
+  'Y' => 'Draw',
+  'Z' => 'Win'
+}
+
+strategies = {
+  'Rock' => 'Paper',
+  'Paper' => 'Scissors',
+  'Scissors' => 'Rock'
+}
+
+points = 0
+lines.each_with_index do |line, index|
+  opponent = definitions[line.chars[0]]
+  response = definitions[line.chars[2]]
+
+  response = case response
+    when 'Lose'
+      strategies[strategies[opponent]] # pick the strategy against the opponent, and then pick the strategy against that
+    when 'Win'
+      strategies[opponent] # pick the strategy against the opponent
+    else # should draw
+      opponent # pick whatever the opponent drew
+   end
+
+  points += strategies.find_index { |k,_| k == response } + 1 # add points based on the index of the move I played
+
+  case response
+  when opponent # draw
+    points += 3
+  when strategies[opponent] # i won
+    points += 6
+  end
+end
+p points
