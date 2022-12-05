b829ee307a195bc51d445f84fee48474254ad4a1
MrWouterNL <wouter@dedroog.com>
2022-12-05 18:53:30 +0100

    Improve code structure

--- day5_1.rb
+++ day5_1.rb
@@ -1,15 +1,15 @@
 lines = File.open('inputs/day5.txt').readlines
 header_lines = lines.select { |l| l.include? '[' }
                     .map { |line| line.chars.select.with_index { |_, i| ((i - 1) % 4).zero? } }
-rows = {1 => [], 2 => [], 3 => [], 4 => [], 5 => [], 6 => [], 7 => [], 8 => [], 9 => []}
+rows = { 1 => [], 2 => [], 3 => [], 4 => [], 5 => [], 6 => [], 7 => [], 8 => [], 9 => [] }
 
 header_lines.reverse.each_with_index do |line, index|
   line.each_with_index do |letter, letter_index|
     rows[letter_index + 1].append(letter) unless letter == ' '
   end
 end
-
 # </parsing>
+# <solution>
 lines = lines.select { |line| line.include? 'move' }
              .map { |line| line.match(/move (\d{1,2}) from (\d) to (\d)/) }
 
@@ -19,9 +19,8 @@ lines.each do |match_data|
   to = match_data[3].to_i
 
   amount.times do
-    to_be_moved = rows[from][rows[from].length-1]
+    rows[to].append(rows[from].last)
     rows[from].pop # remove last item in row
-    rows[to].append(to_be_moved)
   end
 end
-puts rows.keys.map { |key| rows[key][rows[key].length - 1] }.join
+puts rows.values.map(&:last).join
--- day5_2.rb
+++ day5_2.rb
@@ -1,14 +1,15 @@
 lines = File.open('inputs/day5.txt').readlines
 header_lines = lines.select { |l| l.include? '[' }
                     .map { |line| line.chars.select.with_index { |_, i| ((i - 1) % 4).zero? } }
-rows = {1 => [], 2 => [], 3 => [], 4 => [], 5 => [], 6 => [], 7 => [], 8 => [], 9 => []}
+rows = { 1 => [], 2 => [], 3 => [], 4 => [], 5 => [], 6 => [], 7 => [], 8 => [], 9 => [] }
 
 header_lines.reverse.each_with_index do |line, index|
   line.each_with_index do |letter, letter_index|
     rows[letter_index + 1].append(letter) unless letter == ' '
   end
 end
-
+# </parsing>
+# <solution>
 lines = lines.select { |line| line.include? 'move' }
              .map { |line| line.match(/move (\d{1,2}) from (\d) to (\d)/) }
 
@@ -20,4 +21,4 @@ lines.each do |match_data|
   rows[to].concat(rows[from].last(amount))
   rows[from].pop(amount)
 end
-puts rows.keys.map { |key| rows[key][rows[key].length - 1] }.join
+puts rows.values.map(&:last).join
