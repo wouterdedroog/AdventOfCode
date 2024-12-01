7e105a7fcc831acf7a3a5d58efa6d9c9934844aa
Wouter de Droog <wouter@dedroog.com>
2024-12-01 13:04:00 +0100

    ooga booga big brain (dankjewel sting)

--- day1.rb
+++ day1.rb
@@ -1,14 +1,6 @@
 lines = File.open('inputs/day1.txt').readlines
 
-first, second = lines.map { |line| line.split('   ').map(&:to_i) }.transpose
+first, second = lines.map { |line| line.split('   ').map(&:to_i) }.transpose.map(&:sort)
 
+puts "part 1: #{first.map.with_index { |first_min, i| (second[i] - first_min).abs }.sum}"
 puts "part 2: #{first.map { |x| x * second.count(x) }.sum}"
-
-result = first.sort.map do |first_min|
-  second_min = second.min
-  second.delete_at(second.index(second_min))
-
-  (second_min - first_min).abs
-end.sum
-
-puts "part 1: #{result}"
