625a6e539fb688eb076ccdc55e04bb5212ccb113
Wouter de Droog <wouter@dedroog.com>
2024-12-06 23:49:15 +0100

    Clean up code, work on optimising day6_2

--- day6.rb
+++ day6.rb
@@ -3,11 +3,11 @@ require 'set'
 grid = File.open('inputs/day6.txt').readlines
            .map { |line| line.chomp.split('') }
 
-direction = [0, -1].freeze # move up (meaning don't modify X, subtract 1 from Y every move)
-current_position = grid.filter_map
-                     .with_index { |line, index| [line.find_index('^'), index] if line.include? '^' }
-                     .first
-                     .freeze
+start_direction = [0, -1].freeze # move up (meaning don't modify X, subtract 1 from Y every move)
+start_position = grid.filter_map
+                     .with_index { |line, index|
+                       [line.find_index('^'), index, start_direction] if line.include? '^'
+                     }.first.freeze
 
 def move(grid, current_position, direction)
   new_x = current_position[0] + direction[0]
@@ -16,7 +16,7 @@ def move(grid, current_position, direction)
   return nil if new_x.negative? or new_x >= grid[0].length
   return nil if new_y.negative? or new_y >= grid.length
 
-  [new_x, new_y]
+  [new_x, new_y, direction]
 end
 
 def char_at(grid, position)
@@ -28,25 +28,56 @@ def next_direction(current_direction)
   when [0, -1] then [1, 0] # up to right
   when [1, 0] then [0, 1] # right to down
   when [0, 1] then [-1, 0] # down to left
-  else [0, -1] # left to up
+  when [-1, 0] then [0, -1] # left to up
   end
 end
 
-visited_positions = Set.new
-visited_positions.add(current_position)
-while true do
-  old_position = current_position.clone
-  current_position = move(grid, current_position, direction)
-  break if current_position.nil?
-
-  char_at_position = char_at(grid, current_position)
-
-  if char_at_position == '#'
-    direction = next_direction(direction)
-    current_position = move(grid, old_position, direction)
-  end
-
-  visited_positions << current_position
+def is_in_loop(current_position, visited_positions)
+  visited_positions.include? current_position
 end
 
-puts "Arrived after #{visited_positions.uniq.length} movements"
+def solve_grid(grid, start_position, start_direction)
+  visited_positions = Set.new
+  visited_positions.add(start_position)
+
+  current_position = start_position
+  direction = start_direction.clone
+  while true do
+    current_position = move(grid, current_position, direction)
+    break if current_position.nil?
+    return true if is_in_loop(current_position, visited_positions)
+
+    char_at_position = char_at(grid, current_position)
+
+    if char_at_position == '#'
+      # move 1 step back
+      back_direction = next_direction(next_direction(direction))
+      current_position = move(grid, current_position, back_direction)
+
+      direction = next_direction(direction)
+    end
+
+    visited_positions.add(current_position)
+  end
+
+  visited_positions.map { |x| [x[0], x[1]] }.uniq
+end
+
+pt1_visited_positions = solve_grid(grid, start_position, start_direction)
+puts "Part 1: #{pt1_visited_positions.count}"
+
+# Part 2
+loops = grid.map.with_index do |y_line, y_index|
+  y_line.each_with_index.count do |x_char, x_index|
+    next unless x_char == '.'
+    next unless pt1_visited_positions.include? [x_index, y_index]
+
+    new_grid = grid.map(&:dup)
+    new_grid[y_index][x_index] = '#'
+
+    solve_grid(new_grid, start_position, start_direction).is_a? TrueClass
+  end
+end.sum
+
+puts "Part 2: #{loops}"
+
